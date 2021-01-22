fb9e8671e62365700dbfa9e0512ba1b4ac8e448d
sgtSeme4ki <markdurst98@gmail.com>
2021-01-22 16:23:16 +0100

    new Schema with ER-Diagram

--- init/createTableStatements.sql
+++ init/createTableStatements.sql
@@ -22,18 +22,33 @@ create table if not exists risiko(
 	
 create table if not exists steuerungsmaßnahme(
 	steuerungsmaßnahme_id int auto_increment primary key, 
-	steuerungsmaßnahme_name varchar(255) not null, 
+	steuerungsmaßnahme_name varchar(255) not null unique, 
 	effizienz decimal(2, 1) not null,
 	bedeutung decimal(2, 1) not null
 	);
+	
+create table if not exists dienstleisterart(
+	dienstleisterart_id int auto_increment primary key,
+	dienstleister_name varchar(255) not null unique
+	);
 
 -- Relationships
-create table if not exists gefährdung(
-	dienstleister_id int not null, 
-	risiko_id int not null, 
-	primary key (dienstleister_id, risiko_id),
+create table if not exists typisierung(
+	dienstleister_id int not null,
+	dienstleisterart_id int not null,
+	primary key (dienstleister_id, dienstleisterart_id),
 	foreign key (dienstleister_id)
 		references dienstleister(dienstleister_id),
+	foreign key (dienstleisterart_id)
+		references dienstleisterart(dienstleisterart_id)
+		);
+		
+create table if not exists gefährdung(
+	dienstleisterart_id int not null, 
+	risiko_id int not null, 
+	primary key (dienstleisterart_id, risiko_id),
+	foreign key (dienstleisterart_id)
+		references dienstleisterart(dienstleisterart_id),
 	foreign key (risiko_id)
 		references risiko(risiko_id)
 		);
@@ -49,5 +64,5 @@ create table if not exists steuerung(
 		);
 
 
-alter table steuerungsmaßnahme add constraint point_one_to_one check((effizienz <= 1 and effizienz >= 0.1) and (bedeutung <= 1 and bedeutung >= 0.1)
+alter table steuerungsmaßnahme add constraint intervall check((effizienz <= 1 and effizienz >= 0.1) and (bedeutung <= 1 and bedeutung >= 0.1)
 );
