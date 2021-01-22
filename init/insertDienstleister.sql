5d9d4742e88b5f50af5e67432f66f361c1ecce17
sgtSeme4ki <markdurst98@gmail.com>
2021-01-22 16:40:00 +0100

    delete insertions of old schema

--- init/insertDienstleister.sql
+++
@@ -1,9 +0,0 @@
--- entries
-insert into dienstleister
-values (default, 'Schote und Quarz', 'Hinten Links');
-
-insert into dienstleister
-values (default, 'Kutten & Noks', 'Hamburg');
-
-insert into dienstleister
-values (default, 'Placebook', 'Frankfurt a. M.');
