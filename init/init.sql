f6703caa8b36c514e5b22f529b7c3d6d12f26292
sgtSeme4ki <markdurst98@gmail.com>
2021-02-08 14:38:57 +0100

    test daten und er

--- init/init.sql
+++ init/init.sql
@@ -6,7 +6,8 @@ drop table if exists dienstleisterart;
 drop table if exists bewertung_steuerungsmassnahmenahme;
 drop table if exists bewertung_risiko;
 
-drop table if exists ergebnis;
+drop table if exists ergebnis_risiko;
+drop table if exists ergebnis_steuerung;
 
 -- relationship tables
 
@@ -60,14 +61,30 @@ create table if not exists bewertung_risiko(
 	bedeutung int
 	);
 	
-create table if not exists ergebnis(
+create table if not exists ergebnis_risiko(
 	datum datetime,
 	risikoprioritaetszahl int,
-	steuerungsprioritaetszahl int,
 	dienstleister_id int,
+	risiko_id int,
 	primary key (datum, dienstleister_id),
 	foreign key (dienstleister_id)
-		references dienstleister(dienstleister_id)
+		references dienstleister(dienstleister_id),
+	foreign key (risiko_id)
+		references risiko(risiko_id)
+	);
+	
+create table if not exists ergebnis_steuerung(
+	datum datetime,
+	risiko_id int,
+	steuerungsprioritaetszahl int,
+	dienstleister_id int,
+	primary key (datum, dienstleister_id, risiko_id),
+	foreign key (dienstleister_id)
+		references ergebnis_risiko(dienstleister_id),
+	foreign key (datum)
+		references ergebnis_risiko(datum),
+	foreign key (risiko_id)
+		references ergebnis_risiko(risiko_id)
 	);
 
 -- Relationships
@@ -215,6 +232,8 @@ values('Informationsverlust durch unvollständige oder nicht gültige Verzeichni
 insert into risiko(risiko_name)
 values('Unzureichende Überwachung der Einhaltung der Anforderungen der Dienstleister');
 
+-- ----------------------------------------------------------------------------------------------------------------------------------------
+
 insert into steuerungsmassnahme(steuerungsmassnahme_name)
 values('Log-Files: Automatische Erfassung der Assets und dadurch direkte Dokumentation von Veränderungen an der Datenbank');
 
@@ -240,7 +259,7 @@ insert into steuerungsmassnahme(steuerungsmassnahme_name)
 values('Eine Einbruchmeldeanlage ist installiert, so dass Unregelmäßigkeiten schnell gemeldet werden.');
 
 insert into steuerungsmassnahme(steuerungsmassnahme_name)
-values('Es gibt regelmäßigen Kontrollen der Betriebsparameter. Diese werden in Logs dokumentiert um so Veränderungen festzustellen.');
+values('Es gibt regelmäßige Kontrollen der Betriebsparameter. Diese werden in Logs dokumentiert um so Veränderungen festzustellen.');
 
 insert into steuerungsmassnahme(steuerungsmassnahme_name)
 values('Prognose von zukünftigen Kapazitätsanforderungen');
@@ -275,6 +294,89 @@ values('Protokolle oder zusammengefassten Ergebnisse in einem Self-Service Porta
 insert into steuerungsmassnahme(steuerungsmassnahme_name)
 values('Regelmäßige Tests der Wiederherstellungsverfahren (Vorgaben zur maximal tolerierbaren Ausfallzeit und zum maximal zulässigen Datenverlust werden so geprüft)');
 
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Entwicklung eines Authentisierungskonzeptes für IT-Systeme und Anwendungen');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Einsatz eines zentralen Authentisierungsdienstes');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Schutz von Benutzerkennungen mit weitreichenden Berechtigungen');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Geeignete Auswahl von Authentisierungsmechanismen');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Vergabe von Zugriffsrechten');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Regelung des Passwortgebrauchs/der Passwortqualität');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Mehr-Faktor-Authentisierung');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Notfallvorsorge für das Identitäts- und Berechtigungsmanagement-Systemen');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Einweisung aller Mitarbeiter in den Umgang mit Authentisierungsverfahren und – mechanismen');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Regelmäßige Kontrolle der kryptografischen Sicherheitsstandards der Erzeugung, Veränderung und Übermittlung der Schlüssel');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('kontinuierliche Prüfung ob die Verschlüsselung aktiv ist und die genehmigten Ausnahmen eingehalten werden');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Regelmäßige Kontrollen des Systemzustandes auf Kompromittierung und Dokumentation von Unregelmäßigkeiten');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Dokumentation, Kommunikation und Bereitstellung von Richtlinien und Anweisungen mit technischen und organisatorischen Maßnahmen zum Schutz der Datenübertragung');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Geregelter Umgang mit kompromittierten/veralteten Schlüsseln');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Dokumentation und Kommunikation von Ausnahmen zur Verschlüsselung von Daten gemäß eines geregelten Verfahrens sowie Abstimmung des Verfahrens mit Kunden');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Dokumentation und Implementierung technischer Maßnahmen zur Sicherstellung des Nicht-Beitretens von unbekannten Geräten in das physische/virtuelle Netz');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Geeignete Kontrollen zur zeitnahen Identifikation und Reaktion von/auf netzbasierte Angriffe und Dokumentation & Kommunikation dieser');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Verwendung eines übergreifenden SIEM-Systems zur Initialisierung erforderlicher (Gegen-)Maßnahmen');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Separierung Netze nach Vertrauen in verschiedene Netzbereiche');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Konzeption und Konfiguration physischer und virtualisierter Netzumgebungen zur Beschränkung und Überwachung von Verbindungen');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Regelmäßige risikoorientierte Überprüfung der Konzepte zur Überwachung der Verbindung zu (nicht) vertrauenswürdigen Netzen');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Vorhandensein Konzept zur Segregation des Datenverkehrs auf Netzebene & Dokumentation dieses Konzepts');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Zertifizierte Prüfung der Sicherheit über sachverständige Dritte');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Anforderungen an Qualifikation und Kompetenz der Dienstleister');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Genaue Beschreibung, dessen was bereitzustellen ist');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Vereinbarungen zur Leistungsüberwachung der Dienstleister');
+
+insert into steuerungsmassnahme(steuerungsmassnahme_name)
+values('Regelmäßige Überprüfung der Einhaltung der Anforderungen');
+
+-- CC
+
 insert into gefaehrdung(dienstleisterart_id, risiko_id)
 values(1, 1);
 
@@ -305,8 +407,348 @@ values(1, 9);
 insert into gefaehrdung(dienstleisterart_id, risiko_id)
 values(1, 10);
 
--- 11 - 29 risks are missing from 
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 11);
 
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 12);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 13);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 14);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 15);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 16);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 17);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 18);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 19);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 20);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 21);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 22);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 23);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 24);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 25);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 26);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 27);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 28);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(1, 29);
+
+-- RZ
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 1);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 2);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 3);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 4);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 5);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 6);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 7);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 8);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 9);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 10);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 11);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 12);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 13);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 14);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 15);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 16);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 17);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 18);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 19);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 20);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 21);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 22);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 23);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 24);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 25);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 26);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 28);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(2, 29);
+
+-- HW
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(3, 1);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(3, 2);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(3, 3);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(3, 4);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(3, 5);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(3, 7);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(3, 8);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(3, 10);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(3, 11);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(3, 12);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(3, 17);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(3, 25);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(3, 26);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(3, 28);
+
+-- IS
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(4, 3);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(4, 4);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(4, 5);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(4, 7);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(4, 8);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(4, 11);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(4, 12);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(4, 13);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(4, 14);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(4, 15);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(4, 22);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(4, 23);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(4, 24);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(4, 25);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(4, 26);
+
+-- DW
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(5, 5);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(5, 7);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(5, 8);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(5, 10);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(5, 11);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(5, 12);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(5, 22);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(5, 23);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(5, 24);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(5, 25);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(5, 26);
+
+-- SU
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(6, 10);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(6, 11);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(6, 25);
+
+-- CS
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(7, 10);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(7, 11);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(7, 25);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(7, 26);
+
+-- SE
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(8, 11);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(8, 12);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(8, 13);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(8, 14);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(8, 15);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(8, 16);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(8, 17);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(8, 18);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(8, 19);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(8, 20);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(8, 21);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(8, 25);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(8, 26);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(8, 28);
+
+insert into gefaehrdung(dienstleisterart_id, risiko_id)
+values(8, 29);
+
+-- ----------------------------------------------------------------------------------------------------------------------------------------
 -- steuerungsmassnahme - risiko
 
 insert into steuerung(steuerungsmassnahme_id, risiko_id)
@@ -347,3 +789,72 @@ values(9, 9);
 
 insert into steuerung(steuerungsmassnahme_id, risiko_id)
 values(10, 10);	
+
+insert into steuerung(steuerungsmassnahme_id, risiko_id)
+values(11, 10);	
+
+insert into steuerung(steuerungsmassnahme_id, risiko_id)
+values(12, 11);	
+
+insert into steuerung(steuerungsmassnahme_id, risiko_id)
+values(13, 11);	
+
+insert into steuerung(steuerungsmassnahme_id, risiko_id)
+values(14, 11);	
+
+insert into steuerung(steuerungsmassnahme_id, risiko_id)
+values(15, 11);	
+
+insert into steuerung(steuerungsmassnahme_id, risiko_id)
+values(16, 11);	
+
+insert into steuerung(steuerungsmassnahme_id, risiko_id)
+values(17, 11);	
+
+insert into steuerung(steuerungsmassnahme_id, risiko_id)
+values(18, 12);	
+
+insert into steuerung(steuerungsmassnahme_id, risiko_id)
+values(19, 13);
+
+insert into steuerung(steuerungsmassnahme_id, risiko_id)
+values(19, 15);		
+
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(20,14);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(21,16);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(21,21);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(22,16);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(23,16);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(23,21);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(24,16);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(24,20);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(24,21);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(25,17);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(25,18);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(26,18);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(26,21);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(27,18);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(28,19);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(29,20);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(30,22);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(30,23);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(30,24);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(31,22);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(32,22);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(32,23);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(32,24);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(33,22);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(34,23);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(35,24);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(36,25);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(37,25);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(38,25);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(39,26);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(40,26);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(41,26);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(42,27);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(43,27);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(44,28);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(45,28);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(46,29);
+insert into steuerung(steuerungsmassnahme_id, risiko_id) values(47,29);
